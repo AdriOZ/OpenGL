5d1c88b191789175d75669346f131a2511393f89
AdriOZ <adrianozamora@gmail.com>
2022-03-03 20:03:58 +0100

    ImGui

--- src/Application.cpp
+++ src/Application.cpp
@@ -7,6 +7,10 @@
 
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
 #include "Renderer.h"
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
@@ -40,6 +44,13 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 130");
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
 	if (glewInit() != GLEW_OK)
 	{
 		std::cout << "Error" << std::endl;
@@ -72,14 +83,11 @@ int main(void)
 	IndexBuffer ib(indices, 6);
 	glm::mat4 proj = glm::ortho(0.0f, (float)mode->width, 0.0f, (float)mode->height, -1.0f, 1.0f);
 	glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(500.0f, 500.0f, 0.0f));
-	glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(500.0f, 500.0f, 0.0f));
-	glm::mat4 mvp = proj * view * model;
-
 	
 	Shader sh("res/shaders/Basic.shader");
 	sh.Bind();
 	sh.SetUniform1i("u_Texture", 0);
-	sh.SetUniformMat4f("u_MVP", mvp);
+	
 	sh.Unbind();
 
 	Renderer renderer;
@@ -87,6 +95,10 @@ int main(void)
 	Texture te("res/textures/avatar.png");
 	te.Bind();
 
+	int display_w, display_h;
+	glfwGetFramebufferSize(window, &display_w, &display_h);
+
+	glm::vec3 translation = glm::vec3(500.0f, 500.0f, 0.0f);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -95,6 +107,25 @@ int main(void)
 		renderer.Clear();
 
 		renderer.Draw(va, ib, sh);
+		glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+		glm::mat4 mvp = proj * view * model;
+		sh.SetUniformMat4f("u_MVP", mvp);
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		{
+			ImGui::Begin("Hello, world!");
+			ImGui::SliderFloat3("Translation", &translation.x, 0.0f, (float)mode->width);
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			ImGui::End();
+		}
+
+		ImGui::Render();
+		
+		glViewport(0, 0, display_w, display_h);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
@@ -103,6 +134,11 @@ int main(void)
 		glfwPollEvents();
 	}
 
+	ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
 }
