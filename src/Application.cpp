eabb68fadf01b3d0ce23eb6f11dc7ec7e1fe959e
AdriOZ <adrianozamora@gmail.com>
2022-03-03 20:11:25 +0100

    Some tests

--- src/Application.cpp
+++ src/Application.cpp
@@ -31,7 +31,6 @@ int main(void)
 	GLFWmonitor* monitor = glfwGetPrimaryMonitor();
 	const GLFWvidmode* mode = glfwGetVideoMode(monitor);
 
-	std::cout << mode->width << " x " << mode->height;
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(mode->width, mode->height, "OpenGL", NULL, NULL);
 
@@ -49,7 +48,6 @@ int main(void)
 	ImGui::StyleColorsDark();
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
 	ImGui_ImplOpenGL3_Init("#version 130");
-	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 	if (glewInit() != GLEW_OK)
 	{
@@ -61,44 +59,36 @@ int main(void)
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+	VertexArray va;
 	float positions[] = {
 		-50.0f, -50.0f, 0.0f,   0.0f,
 		 50.0f, -50.0f, 100.0f, 0.0f,
 		 50.0f,  50.0f, 100.0f, 100.0f,
 		-50.0f,  50.0f, 0.0f,   100.0f
 	};
-
-	uint32_t indices[] = {
-		0, 1, 2,
-		2, 3, 0
-	};
-
-	VertexArray va;
 	VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 	VertexBufferLayout layout;
 	layout.Push<float>(2);
 	layout.Push<float>(2);
 	va.AddBuffer(vb, layout);
 
+	uint32_t indices[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
 	IndexBuffer ib(indices, 6);
+	Shader sh("res/shaders/Basic.shader");
+
 	glm::mat4 proj = glm::ortho(0.0f, (float)mode->width, 0.0f, (float)mode->height, -1.0f, 1.0f);
 	glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(500.0f, 500.0f, 0.0f));
-	
-	Shader sh("res/shaders/Basic.shader");
-	sh.Bind();
-	sh.SetUniform1i("u_Texture", 0);
-	
-	sh.Unbind();
+	glm::vec3 translation = glm::vec3(500.0f, 500.0f, 0.0f);
+
+	glm::vec3 color = glm::vec3(1.0f, 0.0f, 1.0f);
 
 	Renderer renderer;
 
-	Texture te("res/textures/avatar.png");
-	te.Bind();
-
 	int display_w, display_h;
 	glfwGetFramebufferSize(window, &display_w, &display_h);
-
-	glm::vec3 translation = glm::vec3(500.0f, 500.0f, 0.0f);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -110,6 +100,7 @@ int main(void)
 		glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
 		glm::mat4 mvp = proj * view * model;
 		sh.SetUniformMat4f("u_MVP", mvp);
+		sh.SetUniform4f("u_Color", color.r, color.g, color.b, 1.0f);
 
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
@@ -118,6 +109,7 @@ int main(void)
 		{
 			ImGui::Begin("Hello, world!");
 			ImGui::SliderFloat3("Translation", &translation.x, 0.0f, (float)mode->width);
+			ImGui::SliderFloat3("Color", &color.x, 0.0f, 1.0f);
 			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 			ImGui::End();
 		}
